9b9b996d28c47002e71d8ee31ec4f8b0c77fc1d8
mujtabasaqib <mujtabasaqib99@gmail.com>
2025-02-11 18:25:59 +0500

    initialized employee and claim tokens functionality

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -16,6 +16,24 @@ pub mod tokenvesting {
       Ok(())
   }
 
+  pub fn create_employee_vesting(
+    ctx: Context<CreateEmployeeAccount>,
+    start_time: i64,
+    end_time: i64,
+    total_amount: i64,
+    cliff_time: i64
+) -> Result<()> {
+
+    Ok(())
+}
+
+pub fn claim_tokens(ctx: Context<ClaimTokens>, _company_name: String) -> Result<()> {
+ 
+  Ok(())
+}
+
+
+
 }
 
 #[derive(Accounts)]
@@ -23,7 +41,23 @@ pub mod tokenvesting {
 pub struct CreateVestingAccount<'info> {
 }
 
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct ClaimTokens<'info> {
+   
+}
+
 #[account]
 #[derive(InitSpace, Debug)]
 pub struct VestingAccount {
+}
+
+#[account]
+#[derive(InitSpace, Debug)]
+pub struct EmployeeAccount {
 }
